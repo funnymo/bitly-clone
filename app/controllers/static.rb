d39f1878f8693622e8618d56906751e7611fde01
Morgan <mky.wee@gmail.com>
2016-04-18 15:53:20 +0800

    succeeded in shortening link

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,5 +1,27 @@
+require 'byebug'
+
 get '/' do
+  # puts "[LOG] Getting /"
+  # puts "[LOG] Params: #{params.inspect}"
+  # @urls = Url.all
+  @urls = url.last
   erb :"static/index"
   # erb :root
   # erb :"users/new"
+end
+
+post '/urls' do
+  # create a new url
+  @url = Url.new(long_url: params['long_url'])
+  @url.short_url = "www.nit.com/#{Url.shorten}"
+  # if url.save
+  erb :"static/shortened_url"
+ #  else
+ #   redirect '/'
+ # end
+end
+
+get '/:short_url' do
+  #redirect to appropriate "long" URL
+  redirect "#{url.long_url}"
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,9 @@
+require 'SecureRandom'
+
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+  before_save :shorten
+  def self.shorten
+    @short_url = SecureRandom.hex[0..5]
+  end
 end
