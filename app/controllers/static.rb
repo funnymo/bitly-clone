7d8799c2ba5439eb68aed4f3b8667f5eb1f37db4
Morgan <mky.wee@gmail.com>
2016-04-18 16:51:05 +0800

    assume done

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,27 +1,31 @@
 require 'byebug'
 
 get '/' do
-  # puts "[LOG] Getting /"
-  # puts "[LOG] Params: #{params.inspect}"
-  # @urls = Url.all
+  p Url.all
   @urls = url.last
   erb :"static/index"
-  # erb :root
-  # erb :"users/new"
 end
 
 post '/urls' do
-  # create a new url
-  @url = Url.new(long_url: params['long_url'])
-  @url.short_url = "www.nit.com/#{Url.shorten}"
-  # if url.save
-  erb :"static/shortened_url"
- #  else
- #   redirect '/'
- # end
+  byebug
+  # if Url.where(long_url) == params['long_url']
+    url = Url.new(long_url: params['long_url'])
+    url.short_url = "www.nit.com/#{Url.shorten}"
+    if url.save
+      @url = url
+      erb :"static/shortened_url"
+    else
+      erb :"static/error"
+    end
+  # else
+  #   erb :"static/error"
+  # end
 end
 
 get '/:short_url' do
   #redirect to appropriate "long" URL
-  redirect "#{url.long_url}"
+  y = Url.find_by(short_url: params[:short_url])
+  y.click_count += 1
+  y.save
+  redirect "#{y.long_url}"
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,7 @@ require 'SecureRandom'
 
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-  before_save :shorten
+  # before_create :shorten
   def self.shorten
     @short_url = SecureRandom.hex[0..5]
   end
